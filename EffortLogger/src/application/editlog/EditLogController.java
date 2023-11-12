132d8757ca858fccd06f7be46a79faa2707ccd40
pramathshukla <pramathshukla@localhost>
2023-11-12 16:01:17 -0700

    I have updated the edit functionality. I have made changes to the EditlogController.java file and EditLog.fxml file.

--- EffortLogger/src/application/editlog/EditLogController.java
+++ EffortLogger/src/application/editlog/EditLogController.java
@@ -1,16 +1,65 @@
 package application.editlog;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
 import application.pastlogs.PastLogs;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 
-public class EditLogController {
-	@FXML private Button cancelButton;
+public class EditLogController<Strings> implements Initializable{
+	@FXML 
+	private Button cancelButton;
 	
-	@FXML
-    public void initialize() {
+	@FXML private TextField logNameTextField;
+	
+	@FXML private TextField storyPointsTextField;
+	
+	@FXML private TextField endTimeTextField; 
+	
+	@FXML private TextField startTimeTextField;
+	
+	@FXML private DatePicker datePicker;
+    @FXML
+    private ChoiceBox<String> statusChoiceBox;
+    
+    @FXML
+    private ChoiceBox<String> categoryChoiceBox;
+    
+    @FXML 
+    private ChoiceBox<String> ProjectNameChoiceBox;
+    
+    @FXML 
+    private ChoiceBox<String> LifeCycleChoiceBox;
+    
+    
+    
+    @FXML
+    private Button saveButton;
+    
+    public void initialize(URL arg0, ResourceBundle arg1) {
         setupCancelButton();
+        setupSaveButton();
+        
+        statusChoiceBox.getItems().add((String) "In Progress");
+    	statusChoiceBox.getItems().add((String) "Completed");
+    	
+    	categoryChoiceBox.getItems().add((String) "Effort");
+    	categoryChoiceBox.getItems().add((String) "Defect");
+    	
+    	
     }
 
 	@FXML
@@ -20,5 +69,72 @@ public class EditLogController {
             pastLogs.show((Stage) cancelButton.getScene().getWindow());
         });
     }
+	
+	@FXML
+    private void setupSaveButton() {
+        saveButton.setOnAction(event -> {
+            try {
+                String lognameToEdit = logNameTextField.getText();
+                String newStatus = statusChoiceBox.getValue();
+                String newDate = datePicker.getValue().toString();
+                String newStartTime = startTimeTextField.getText();
+                String newEndTime = endTimeTextField.getText();
+                String newCategory = categoryChoiceBox.getValue();
+                String newStoryPoints = storyPointsTextField.getText();
+                updateDatabase(lognameToEdit, newStatus, newDate, newStartTime, newEndTime, newCategory, newStoryPoints);
+                System.out.println("Database updated successfully.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+	private void updateDatabase(String lognameToEdit, String newStatus, String newDate, String newStartTime, String newEndTime, String newCategory, String newStoryPoints) throws IOException {
+	    String filePath = "EffortLogger/src/database/logs.txt"; // Replace with your actual file path
+
+	    // Read the content of the file
+	    List<String> lines = new ArrayList<>();
+	    try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+	        String line;
+	        while ((line = reader.readLine()) != null) {
+	            lines.add(line);
+	        }
+	    }
+
+	    // Find and modify the desired entry
+	    for (int i = 0; i < lines.size(); i++) {
+	        String line = lines.get(i);
+	        if (line.contains("logname:" + lognameToEdit)) {
+	            System.out.println("Found logname to edit: " + lognameToEdit);
+
+	            // Construct the updated line
+	            String updatedLine = "logname:" + lognameToEdit + ", Date:" + newDate + ", Status:" + newStatus +
+	                    ", Start Time:" + newStartTime + ", End Time:" + newEndTime +
+	                    ", Project Name:testname" + ", Category:" + newCategory + ", Story Points:" + newStoryPoints;
+
+	            // Replace the entire line with the updated line
+	            lines.set(i, updatedLine);
+
+	            System.out.println("Updated data: " + updatedLine);
+
+	            break; // Break out of the loop once the entry is modified
+	        }
+	    }
+
+	    // Write the updated content back to the file
+	    try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+	        for (String line : lines) {
+	            writer.write(line);
+	            writer.newLine();
+	        }
+	    }
+	}
+
+	/*public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}*/
+	
+	//String database = "src/database/accounts.txt";
 
 }
